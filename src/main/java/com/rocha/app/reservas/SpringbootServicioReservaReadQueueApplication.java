2eef90e71a2dce8a5c601dd92cd8ccb49dcbaff1
Luis Rocha <luis.rocha@pragma.com.co>
2021-08-16 18:32:13 -0500

    agregando etiqueta Euroka client y eliminando propiedades en el archivo application.yml

--- src/main/java/com/rocha/app/reservas/SpringbootServicioReservaReadQueueApplication.java
+++ src/main/java/com/rocha/app/reservas/SpringbootServicioReservaReadQueueApplication.java
@@ -3,7 +3,9 @@ package com.rocha.app.reservas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EntityScan({"com.rocha.app.commons.model"})
 public class SpringbootServicioReservaReadQueueApplication {
